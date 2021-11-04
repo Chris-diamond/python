d22e8f5528ed2ebbd5515533e8cd38ef58add688
Chris-diamond <abbanchristabel202@gmail.com>
2021-11-04 15:47:56 +0000

    Nurses' chart

---
+++ main.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5,6,7,8]
+y=[0,1,0,2,0,5,0,6]
+plt.title('Title')
+plt.plot(x,y)
+plt.xlabel('Time')
+
+
+x=[1,2,3,4,5,6,5,8]
+y=[0,1,0,4,0,5,0,6]
+plt.title('RST')
+plt.plot(x,y)
+plt.xlabel('Time')
+plt.show()
---
+++ nurses.py
@@ -0,0 +1,13 @@
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(type(arr))
+
+arr = np.array(42)
+
+print(arr)
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
